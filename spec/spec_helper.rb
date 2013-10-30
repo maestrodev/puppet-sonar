6f773c8dd329f75f990c266be3b9909683a8f02c
Karl M. Davis <karl@justdavis.com>
2013-10-29 23:04:23 -0400

    Deleted the spec tests, as they're no longer valid.

--- spec/spec_helper.rb
+++
@@ -1,8 +0,0 @@
-require 'puppetlabs_spec_helper/module_spec_helper'
-
-RSpec.configure do |c|
-  c.before(:each) do
-    Puppet::Util::Log.level = :warning
-    Puppet::Util::Log.newdestination(:console)
-  end
-end
